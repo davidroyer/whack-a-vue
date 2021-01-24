1f52b51a344a26d9c9aba01f2854cc34c79b3c12
David Royer <droyer01@gmail.com>
2021-01-23 22:29:15 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
-  purge: ['./**/*.vue'],
+  // purge: ['./**/*.vue'],
 
   theme: {
     extend: {
