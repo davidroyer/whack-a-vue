413b158b46d0ab89b6d620e141a21ed116668916
David Royer <droyer01@gmail.com>
2021-01-23 23:14:18 -0500

    chore: update

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as GameHole } from '../../components/GameHole.vue'
-export { default as GameInfo } from '../../components/GameInfo.vue'
-
-export const LazyGameHole = import('../../components/GameHole.vue' /* webpackChunkName: "components/game-hole" */).then(c => c.default || c)
-export const LazyGameInfo = import('../../components/GameInfo.vue' /* webpackChunkName: "components/game-info" */).then(c => c.default || c)
--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['dev-helper'] = require('../middleware/dev-helper.js')
-middleware['dev-helper'] = middleware['dev-helper'].default || middleware['dev-helper']
-
-export default middleware
